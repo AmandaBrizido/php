667bcb1669c4ac8bd68c4c84df7db09c0cd41cf3
Amanda Brízido <brizidoamanda@gmail.com>
2025-10-09 13:57:21 -0300

    Create exemplo_foreach.php

---
+++ exemplo_foreach.php
@@ -0,0 +1,8 @@
+<?php
+$pessoa = ["nome" => "Márcia", "idade" => 18, "cidade" => "São Paulo"];
+
+foreach ($pessoa as $key => $value) {
+    echo "Os dados da $pessoa[nome] são: $key $value<br>";
+}
+
+?>
